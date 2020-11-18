3f702028d393e1af0476a8316a5ff9b7c30d826e
Xenia-Io <ksenia_io@hotmail.com>
2020-11-18 15:31:15 -0500

    delete class Tuner()

--- models/vae_model.py
+++ models/vae_model.py
@@ -171,59 +171,6 @@ class VAE(tf.keras.Model):
         return word_model.wv.index2word[idx]
 
 
-class MyTuner(kt.Tuner):
-
-    def run_trial(self, trial, train_ds):
-        hp = trial.hyperparameters
-
-        train_ds = train_ds.batch(
-            hp.Int('batch_size', 32, 128, step=32, default=64))
-
-        model = self.hypermodel.build(trial.hyperparameters)
-        lr = hp.Float('learning_rate', 1e-4, 1e-2, sampling='log', default=1e-3)
-        optimizer = tf.keras.optimizers.Adam(lr)
-        epoch_loss_metric = tf.keras.metrics.Mean()
-
-        @tf.function
-        def run_train_step(data):
-            images = tf.dtypes.cast(data['image'], 'float32') / 255.
-            labels = data['label']
-            with tf.GradientTape() as tape:
-                logits = model(images)
-                loss = tf.keras.losses.sparse_categorical_crossentropy(
-                    labels, logits)
-                # Add any regularization losses.
-                if model.losses:
-                    loss += tf.math.add_n(model.losses)
-                gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            epoch_loss_metric.update_state(loss)
-            return loss
-
-        # `self.on_epoch_end` reports results to the `Oracle` and saves the
-        # current state of the Model. The other hooks called here only log values
-        # for display but can also be overridden. For use cases where there is no
-        # natural concept of epoch, you do not have to call any of these hooks. In
-        # this case you should instead call `self.oracle.update_trial` and
-        # `self.oracle.save_model` manually.
-        for epoch in range(10):
-            print('Epoch: {}'.format(epoch))
-
-            self.on_epoch_begin(trial, model, epoch, logs={})
-            for batch, data in enumerate(train_ds):
-                self.on_batch_begin(trial, model, batch, logs={})
-                batch_loss = float(run_train_step(data))
-                self.on_batch_end(trial, model, batch, logs={'loss': batch_loss})
-
-                if batch % 100 == 0:
-                    loss = epoch_loss_metric.result().numpy()
-                    print('Batch: {}, Average Loss: {}'.format(batch, loss))
-
-            epoch_loss = epoch_loss_metric.result().numpy()
-            self.on_epoch_end(trial, model, epoch, logs={'loss': epoch_loss})
-            epoch_loss_metric.reset_states()
-
-
 if __name__ == "__main__":
     tf.config.experimental_run_functions_eagerly(True)
     print(tf.__version__)
