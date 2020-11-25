83800d681c95f8d71fb98f39075519c696587bdd
Xenia-Io <ksenia_io@hotmail.com>
2020-11-24 19:28:19 -0500

    fixing dataset split for classifiers

--- models/vae_model.py
+++ models/vae_model.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Model, Sequential
 from sklearn.ensemble import (RandomForestClassifier, ExtraTreesClassifier,
                               AdaBoostClassifier)
 from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, regularizers
 from tensorflow.keras.optimizers import SGD
 from kerastuner.tuners import RandomSearch
@@ -185,8 +185,7 @@ class VAE(tf.keras.Model):
 
 def plot_decision_boundary(mse_test_data, test_y):
     # Parameters
-    n_classes = 3
-    n_estimators = 30
+    n_estimators = 20
     cmap = plt.cm.RdYlBu
     plot_step = 0.02  # fine step width for decision surface contours
     plot_step_coarser = 0.5  # step widths for coarse classifier guesses
@@ -206,18 +205,27 @@ def plot_decision_boundary(mse_test_data, test_y):
         idx = np.arange(mse_test_data.shape[0])
         np.random.seed(RANDOM_SEED)
         np.random.shuffle(idx)
-        mse_test_data = mse_test_data[idx]
+        X = mse_test_data[idx]
         y = test_y[idx]
+        # idx_ = np.arange(mse_train_data.shape[0])
+        # np.random.seed(RANDOM_SEED)
+        # np.random.shuffle(idx_)
+        # X_train = mse_train_data[idx_]
+        # y_train = train_y[idx_]
 
         # Standardize
-        mean = mse_test_data.mean(axis=0)
-        std = mse_test_data.std(axis=0)
-        mse_test_data = (mse_test_data - mean) / std
+        mean = X.mean(axis=0)
+        std = X.std(axis=0)
+        X = (X - mean) / std
+
+        # mean_ = X_train.mean(axis=0)
+        # std_ = X_train.std(axis=0)
+        # X_train = (X_train - mean_) / std_
 
         # Train
-        model.fit(mse_test_data, y)
+        model.fit(X, y)
 
-        scores = model.score(mse_test_data, y)
+        scores = model.score(X, y)
         # Create a title for each column and the console by using str() and
         # slicing away useless parts of the string
         model_title = str(type(model)).split(
@@ -237,8 +245,8 @@ def plot_decision_boundary(mse_test_data, test_y):
 
         # Now plot the decision boundary using a fine mesh as input to a
         # filled contour plot
-        x_min, x_max = mse_test_data[:, 0].min() - 1, mse_test_data[:, 0].max() + 1
-        y_min, y_max = mse_test_data[:, 1].min() - 1, mse_test_data[:, 1].max() + 1
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
         xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                              np.arange(y_min, y_max, plot_step))
 
@@ -269,20 +277,168 @@ def plot_decision_boundary(mse_test_data, test_y):
         Z_points_coarser = model.predict(np.c_[xx_coarser.ravel(),
                                                yy_coarser.ravel()]
                                          ).reshape(xx_coarser.shape)
+
         cs_points = plt.scatter(xx_coarser, yy_coarser, s=15,
                                 c=Z_points_coarser, cmap=cmap,
                                 edgecolors="none")
 
+
         # Plot the training points, these are clustered together and have a
         # black outline
-        plt.scatter(mse_test_data[:, 0], mse_test_data[:, 1], c=y,
-                    cmap=ListedColormap(['r', 'y', 'b']),
+        plt.scatter(X[:, 0], X[:, 1], c=y,
+                    cmap=ListedColormap(['r', 'b']),
                     edgecolor='k', s=20)
         plot_idx += 1  # move on to the next plot in sequence
 
     plt.suptitle("Classifiers", fontsize=12)
     plt.axis("tight")
     plt.tight_layout(h_pad=0.2, w_pad=0.2, pad=2.5)
+    plt.show()
+
+
+def plot_decision_boundary_2(mse_train_data, y_train_, mse_test_data, y_test_):
+    # Parameters
+    n_estimators = 20
+    cmap = plt.cm.RdYlBu
+    plot_step = 0.02  # fine step width for decision surface contours
+    plot_step_coarser = 0.5  # step widths for coarse classifier guesses
+    RANDOM_SEED = 13  # fix the seed on each iteration
+
+    plot_idx = 1
+
+    models = [DecisionTreeClassifier(max_depth=None),
+              RandomForestClassifier(n_estimators=n_estimators),
+              ExtraTreesClassifier(n_estimators=n_estimators),
+              AdaBoostClassifier(DecisionTreeClassifier(max_depth=3),
+                                 n_estimators=n_estimators)]
+
+    for model in models:
+
+        # Shuffle
+        idx = np.arange(mse_test_data.shape[0])
+        np.random.seed(RANDOM_SEED)
+        np.random.shuffle(idx)
+        X = mse_test_data[idx]
+        y = y_test_[idx]
+        idx_ = np.arange(mse_train_data.shape[0])
+        np.random.seed(RANDOM_SEED)
+        np.random.shuffle(idx_)
+        X_train = mse_train_data[idx_]
+        y_train = y_train_[idx_]
+
+        # Standardize
+        mean = X.mean(axis=0)
+        std = X.std(axis=0)
+        X = (X - mean) / std
+
+        mean_ = X_train.mean(axis=0)
+        std_ = X_train.std(axis=0)
+        X_train = (X_train - mean_) / std_
+
+        # Train
+        model.fit(X_train, y_train)
+
+        scores = model.score(X, y)
+        # Create a title for each column and the console by using str() and
+        # slicing away useless parts of the string
+        model_title = str(type(model)).split(
+            ".")[-1][:-2][:-len("Classifier")]
+
+        model_details = model_title
+        if hasattr(model, "estimators_"):
+            model_details += " with {} estimators".format(
+                len(model.estimators_))
+        print(model_details, " has a score of ", scores)
+
+        plt.subplot(2, 2, plot_idx)
+
+        if plot_idx <= len(models):
+            # Add a title at the top of each column
+            plt.title(model_title, fontsize=9)
+
+        # Now plot the decision boundary using a fine mesh as input to a
+        # filled contour plot
+        x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                             np.arange(y_min, y_max, plot_step))
+
+        # Plot either a single DecisionTreeClassifier or alpha blend the
+        # decision surfaces of the ensemble of classifiers
+        if isinstance(model, DecisionTreeClassifier):
+            Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+            Z = Z.reshape(xx.shape)
+            cs = plt.contourf(xx, yy, Z, cmap=cmap)
+        else:
+            # Choose alpha blend level with respect to the number
+            # of estimators
+            # that are in use (noting that AdaBoost can use fewer estimators
+            # than its maximum if it achieves a good enough fit early on)
+            estimator_alpha = 1.0 / len(model.estimators_)
+            for tree in model.estimators_:
+                Z = tree.predict(np.c_[xx.ravel(), yy.ravel()])
+                Z = Z.reshape(xx.shape)
+                cs = plt.contourf(xx, yy, Z, alpha=estimator_alpha, cmap=cmap)
+
+        # Build a coarser grid to plot a set of ensemble classifications
+        # to show how these are different to what we see in the decision
+        # surfaces. These points are regularly space and do not have a
+        # black outline
+        xx_coarser, yy_coarser = np.meshgrid(
+            np.arange(x_min, x_max, plot_step_coarser),
+            np.arange(y_min, y_max, plot_step_coarser))
+        Z_points_coarser = model.predict(np.c_[xx_coarser.ravel(),
+                                               yy_coarser.ravel()]
+                                         ).reshape(xx_coarser.shape)
+
+        cs_points = plt.scatter(xx_coarser, yy_coarser, s=15,
+                                c=Z_points_coarser, cmap=cmap,
+                                edgecolors="none")
+
+
+        # Plot the training points, these are clustered together and have a
+        # black outline
+        plt.scatter(X[:, 0], X[:, 1], c=y,
+                    cmap=ListedColormap(['r', 'b']),
+                    edgecolor='k', s=20)
+        plot_idx += 1  # move on to the next plot in sequence
+
+    plt.suptitle("Classifiers", fontsize=12)
+    plt.axis("tight")
+    plt.tight_layout(h_pad=0.2, w_pad=0.2, pad=2.5)
+    plt.show()
+
+
+def plot_decision_surfaces(mse_test_data, test_y, mse_train_data, train_y):
+    names = ["Decision Tree", "Random Forest", "ExtraTrees"]
+    # Creating a Python List with our three Tree classifiers
+    treeclassifiers = [
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=20, max_features=1),
+        ExtraTreesClassifier()]
+
+    figure = plt.figure(figsize=(12, 10))
+    h = 0.02
+    i = 1
+
+    x_min, x_max = mse_train_data[:, 0].min() - .5, mse_train_data[:, 0].max() + .5
+    y_min, y_max = mse_train_data[:, 1].min() - .5, mse_train_data[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # just plot the dataset first
+    cm = plt.cm.jet
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax = plt.subplot(111)
+    # Plot the training points
+    ax.scatter(mse_train_data[:, 0], mse_train_data[:, 1], c=train_y, cmap=cm_bright, alpha=0.7)
+    print(train_y)
+    # and testing points
+    # ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
+
+    i += 1
+
+    # figure.subplots_adjust(left=.02, right=.98)
     plt.show()
 
 
@@ -338,7 +494,7 @@ if __name__ == "__main__":
     ax.set_xlabel('Epoch')
     ax.set_ylabel("Loss")
     ax.legend(loc='upper right')
-    plt.show()
+    # plt.show()
 
     # plot_label_clusters(vae.encoder, train_x, train_y)
 
@@ -354,33 +510,97 @@ if __name__ == "__main__":
     # print("test_y sample[102]: ", test_y[102])
     # print(type(reconstructions), type(test_x))
     # print(type(reconstructions[0]), type(test_x[0]))
-    encoded_inputs = vae.encoder.predict(train_x)
-    reconstructions_train = vae.decoder.predict(encoded_inputs)
-    mse_train = np.mean(np.power(train_x - reconstructions_train, 2), axis=1)
+    train_val_x = np.concatenate((train_x, val_x), axis=0)
+    encoded_inputs = vae.encoder.predict(train_val_x)
+    reconstructions_train_val = vae.decoder.predict(encoded_inputs)
+    mse_train_val = np.mean(np.power(train_val_x - reconstructions_train_val, 2), axis=1)
     mse_test = np.mean(np.power(test_x - reconstructions, 2), axis=1)
-    mse_test_data = np.column_stack((range(len(mse_test)), mse_test))
+    # mse_test_data = np.column_stack((range(len(mse_test)), mse_test))
+    # mse_train_data = np.column_stack((range(len(mse_train)), mse_train))
+
+    mse_all = np.concatenate((mse_train_val, mse_test), axis=0)
+    mse_all_data = np.column_stack((range(len(mse_all)), mse_all))
+    y_train_val = np.concatenate((train_y, val_y), axis=0)
+    y_all = np.concatenate((y_train_val, test_y), axis=0)
+    for i in range(len(y_all)):
+        if (y_all[i] == 1):
+            print("i = ", i ," is label ", y_all[i])
+
+    print("mse_all shape: ", mse_all_data.shape)
+    print("y_all len: ", len(y_all))
+
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=80)
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax.scatter(mse_all_data[:, 0], mse_all_data[:, 1], c=y_all, cmap=cm_bright, alpha=0.6)
+    plt.title("2D plot MSE points grouped by class")
+    # plt.show()
+
 
     # scatter's x & y
-    clean_x, clean_y = mse_test_data[test_y == 0][:, 0], mse_test_data[test_y == 0][:, 1]
-    fraud_x, fraud_y = mse_test_data[test_y == 1][:, 0], mse_test_data[test_y == 1][:, 1]
+    clean_x, clean_y = mse_all_data[y_all == 0][:, 0], mse_all_data[y_all == 0][:, 1]
+    fraud_x, fraud_y = mse_all_data[y_all == 1][:, 0], mse_all_data[y_all == 1][:, 1]
     print("clean x,y : ", clean_x, clean_y)
     print("fraud x,y : ", fraud_x, fraud_y)
 
-    # Create a Gaussian Classifier
-    clf = RandomForestClassifier(max_depth=4, n_estimators=20)
+    # scatter's x & y
 
-    print(train_x.shape, mse_train.shape, train_y.shape)
+    # clean = mse_all_data[y_all == 0][:,:]
+    # clean_y = y_all[y_all == 0]
+    # fraud = mse_all_data[y_all == 1][:,:]
+    # fraud_y = y_all[y_all == 1]
+    # print("clean x,y : ", type(clean), clean.shape)
+    # print("fraud x,y : ", type(fraud), fraud.shape)
+    # print(clean_y, len(clean_y))
+    # print(fraud_y, len(fraud_y))
+    # num_train = int(0.7 * clean.shape[0])
+    # _mse_train_ =
+    _df = pd.DataFrame({'mse': mse_all, 'labels': y_all})
+    fraud = _df[_df.labels == 1]
+    clean = _df[_df.labels == 0]
+
+
+    num_train = int(0.6 * clean.shape[0])
+
+    partition_idx = len(fraud.index) / 2
+    print(len(fraud.index))
+    df_fraud_train = fraud.iloc[0:int(partition_idx), :]
+    print("debug : ", len(df_fraud_train.index))
+    df_copy_train = pd.concat([clean.iloc[0:num_train], df_fraud_train])
+
+    print(df_copy_train.shape)
+    df_copy_test = pd.concat([clean.iloc[num_train:], fraud])
+    print("XENIA 1 \n", df_copy_train.shape)
+    print("XENIA 2 \n", df_copy_test.shape)
+    # clf = RandomForestClassifier(max_depth=4, n_estimators=20)
+    mse_train = df_copy_train.mse.values
+    mse_train_data = np.column_stack((range(len(mse_train)), mse_train))
+    y_train_ = df_copy_train.labels.values
+    print(",,,, ", mse_train_data.shape, type(mse_train_data))
+    print("---- ", len(y_train_), type(y_train_.tolist()), y_train_.tolist())
+    # for i in range(len(y_train_.tolist())):
+    #     print(i, " ", (y_train_.tolist())[(i)])
+
+    mse_test = df_copy_test.mse.values
+    mse_test_data = np.column_stack((range(len(mse_test)), mse_test))
+    y_test_ = df_copy_test.labels.values
+    print(",,,, ", mse_test_data.shape, type(mse_test_data))
+    print("---- ", len(y_test_), type(y_test_.tolist()), y_test_.tolist())
+    for i in range(len(y_test_.tolist())):
+        print(i, " ", (y_test_.tolist())[(i)])
+
     # Train the model using the training set
-    clf.fit(mse_train.reshape(-1, 1), train_y)
-
-    # prediction on test set
-    y_pred = clf.predict(mse_test.reshape(-1, 1))
-
-    # Model Accuracy, how often is the classifier correct?
-    print("RandomForest classifier's Accuracy: ", metrics.accuracy_score(test_y, y_pred))
+    # clf.fit(mse_all_data, )
+    #
+    # # prediction on test set
+    # y_pred = clf.predict(mse_test.reshape(-1, 1))
+    #
+    # # Model Accuracy, how often is the classifier correct?
+    # print("RandomForest classifier's Accuracy: ", metrics.accuracy_score(test_y, y_pred))
 
     # Plot decision boundary
-    plot_decision_boundary(mse_test_data, test_y)
+    plot_decision_boundary(mse_all_data, y_all)
+    plot_decision_boundary_2(mse_train_data, y_train_, mse_test_data, y_test_)
+    # plot_decision_surfaces(mse_test_data, test_y, mse_train_data, train_y)
 
     exit(0)
 
